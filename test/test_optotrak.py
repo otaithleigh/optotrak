ce3c41d016c2de9c0cccb48428b82951c2b8af47
Peter Talley <peterctalley@gmail.com>
2022-06-14 15:38:54 -0400

    Trim test file so output fits in README

--- test/test_optotrak.py
+++ test/test_optotrak.py
@@ -9,8 +9,8 @@ TESTROOT = Path(__file__).parent
 
 def test_load_optotrak():
     data = load_optotrak(TESTROOT / 'test_optotrak.tsv', delimiter='\t')
-    assert_array_equal(data.loc[24, 'Column4'],
-                       [72.362426758, -320.086181641, -2410.855712891])
-    assert data.attrs['Count'] == 500
+    assert_array_equal(data.loc[7, 'Beam2'],
+                       [349.382965088, -561.469482422, -2551.994140625])
+    assert data.attrs['Count'] == 10
     assert data.attrs['Frequency'] == 50.0
     assert data.attrs['Units'] == 'mm'
