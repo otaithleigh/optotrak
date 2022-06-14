4b2ac0771635520f26c95e7da3d00f6e87c20a25
Peter Talley <peterctalley@gmail.com>
2022-06-14 15:16:33 -0400

    Use assert_array_equal instead of assert allclose

--- test/test_optotrak.py
+++ test/test_optotrak.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-import numpy as np
+from numpy.testing import assert_array_equal
 
 from optotrak import load_optotrak
 
@@ -9,8 +9,8 @@ TESTROOT = Path(__file__).parent
 
 def test_load_optotrak():
     data = load_optotrak(TESTROOT / 'test_optotrak.tsv', delimiter='\t')
-    assert np.allclose(data.loc[24, 'Column4'],
-                       [72.362427, -320.086182, -2410.855713])
+    assert_array_equal(data.loc[24, 'Column4'],
+                       [72.362426758, -320.086181641, -2410.855712891])
     assert data.attrs['Count'] == 500
     assert data.attrs['Frequency'] == 50.0
     assert data.attrs['Units'] == 'mm'
